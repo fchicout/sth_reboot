de204d5612fed611ed6efb1322fb641c8ba137bc
Fabio Chicout <fchicout@gmail.com>
2016-03-21 16:47:09 -0300

    first draft of jump

--- src/base/chars/sonic.lua
+++ src/base/chars/sonic.lua
@@ -19,13 +19,11 @@ end
 
 
 function Sonic:loadRunQuads()
-  -- TODO: load the quads of a sonic run
   self.quads["run"]={};
   self.quads["run"][0]=love.graphics.newQuad(555, 0, 43, 46, self.image:getDimensions());
 end
 
 function Sonic:loadJumpQuads()
-  -- TODO: load the quads of a jump
   self.quads["jump"]={};
   self.quads["jump"][0]=love.graphics.newQuad(462, 87, 25, 46, self.image:getDimensions());
   self.quads["jump"][1]=love.graphics.newQuad(487, 102, 31, 31, self.image:getDimensions());
@@ -36,6 +34,7 @@ end
 
 function Sonic:loadCrouchQuads()
   self.quads["crouch"]={}
+  -- This first (quads["crouch"][0]) may be used to refine the movement.
   self.quads["crouch"][0]=love.graphics.newQuad(365, 4, 27, 46, self.image:getDimensions());
   self.quads["crouch"][1]=love.graphics.newQuad(392, 4, 28, 46, self.image:getDimensions());
 end
@@ -87,17 +86,34 @@ function Sonic:draw()
 end
 
 function Sonic:update(dt)
-  if love.keyboard.isDown("left") then
+  
+  if self.vy == 0  and (love.keyboard.isDown("d") or love.keyboard.isDown("D") ) then
+    -- Jump implementation. Needs to fix quad animation
+    self.state = "jump";
+    self.vy = self.jump_height;
+    return;
+  end
+  if self.vy ~= 0 then 
+    self.y = self.y + self.vy*dt;
+    self.vy = self.vy - self.gravity *dt;
+    
+    if self.y < 0 then
+      self.vy = 0;
+      self.y = 0;
+    end
+  elseif love.keyboard.isDown("left") then
+    -- Walk left
     self.state = "walk";
     self.orientation = "left";
   elseif love.keyboard.isDown("right") then
+    -- Walk right
     self.state = "walk";
     self.orientation = "right";
-  elseif love.keyboard.isDown("d") or love.keyboard.isDown("D") then
-    self.state = "jump";
   elseif love.keyboard.isDown("down") then
+    -- Crouch
     self.state = "crouch";
   else 
+    -- Stay idle
     self.state = "idle";
   end
 
@@ -132,20 +148,3 @@ function Sonic:update(dt)
     self.iteration = 1;
   end
 end
-
--- TODO: Jump is complex. Needs to activate over walking (no matter if its right or left),
-
---  elseif self.state == "jump" then
---    self.animationTimer = self.animationTimer + dt;
---    if (self.animationTimer > 0.2) then
---      self.animationTimer = 0.1;
---      self.iteration=self.iteration+1;
---      self.y = self.y + self.vy*dt;
---      self.vy = self.vy - self.gravity*dt;
---      if self.iteration > table.getn(self.quads["jump"]) then
---        self.iteration = 1;
---        --TODO: Develop animation of sonic running!
---      end
---    end
---  end
---end
